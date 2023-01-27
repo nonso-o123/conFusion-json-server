bb3f4fe5b1e991c477f278fddf4b87b83cd4067e
nonso_o123 <cgobidike@gmail.com>
2023-01-27 14:39:39 -0500

    initial commit

---
+++ public/precache-manifest.2928299364c584d94b9a67d65b27c5e6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "272451f19ecb2aa7af9d990a0b3c1171",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dabb023faac170d5b12",
+    "url": "/static/css/2.1bd367ea.chunk.css"
+  },
+  {
+    "revision": "13d05138843fd72197af",
+    "url": "/static/css/main.ee7379c5.chunk.css"
+  },
+  {
+    "revision": "6dabb023faac170d5b12",
+    "url": "/static/js/2.08d24e07.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.08d24e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13d05138843fd72197af",
+    "url": "/static/js/main.d2a9a8b5.chunk.js"
+  },
+  {
+    "revision": "84926f692196015996db",
+    "url": "/static/js/runtime-main.c71f3b90.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
